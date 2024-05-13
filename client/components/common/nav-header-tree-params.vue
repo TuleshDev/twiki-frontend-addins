1a275319f145c2f06b1e7738c73596bb42c3e2df
tulesh <tulesh.hub@gmail.com>
2024-05-13 18:25:41 +0500

    Moved the 'nav-header-tree-params.vue' component from the 'twiki' project

---
+++ client/components/common/nav-header-tree-params.vue
@@ -0,0 +1,24 @@
+<template lang='pug'>
+  .v-toolbar__content
+    div
+      v-tooltip(bottom)
+        template(v-slot:activator='{ on }')
+          v-btn(icon, tile, height='64', v-on='on', @click='subpageNew', :aria-label='$t(`common:header.newSubpage`)')
+            v-icon(color='grey') mdi-text-box-plus
+        span {{$t('common:header.newSubpage')}}
+    v-divider(vertical)
+</template>
+
+<script>
+export default {
+  name: 'NavHeaderTreeParams',
+  created() {
+    console.log("'nav-header-tree-params.vue' component loaded")
+  },
+  methods: {
+    subpageNew () {
+      this.$emit('setFlag')
+    }
+  }
+}
+</script>
--- client/index.js
+++ client/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 
+import NavHeaderTreeParams from './components/common/nav-header-tree-params.vue'
 import PageNavTreeParams from './themes/default/components/page-nav-tree-params.vue'
 
 export {
+  NavHeaderTreeParams,
   PageNavTreeParams
 }
