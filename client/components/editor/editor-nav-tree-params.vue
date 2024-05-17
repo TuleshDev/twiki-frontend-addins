3baf78e005249d41d5c8252b8c4cb32faa5abe02
tulesh <tulesh.hub@gmail.com>
2024-05-17 20:19:50 +0500

    Moved the 'editor-nav-tree-params.vue' component from the 'twiki' project

---
+++ client/components/editor/editor-nav-tree-params.vue
@@ -0,0 +1,40 @@
+<template lang='pug'>
+</template>
+
+<script>
+import VueRouter from 'vue-router'
+
+const router = new VueRouter({
+  mode: 'history',
+  base: '/e'
+})
+
+export default {
+  name: 'EditorNavTreeParams',
+  computed: {
+    mutation1() {
+      return '$level: Int\n$parentSectionId: Int'
+    },
+    mutation2() {
+      return 'level: $level\nparentSectionId: $parentSectionId'
+    },
+    variables() {
+      return {
+        level: this.$store.get('page/level') || 0,
+        parentSectionId: this.$store.get('page/parentSectionId') || 0
+      }
+    }
+  },
+  router,
+  created() {
+    if (this.$route.query.level) {
+      this.$store.set('page/level', parseInt(this.$route.query.level, 10))
+    }
+    if (this.$route.query.parentSectionId) {
+      this.$store.set('page/parentSectionId', parseInt(this.$route.query.parentSectionId, 10))
+    }
+
+    console.log("'editor-nav-tree-params.vue' component loaded")
+  }
+}
+</script>
--- client/index.js
+++ client/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 
 import NavHeaderTreeParams from './components/common/nav-header-tree-params.vue'
+import EditorNavTreeParams from './components/editor/editor-nav-tree-params.vue'
 import PageNavTreeParams from './themes/default/components/page-nav-tree-params.vue'
 
 export {
   NavHeaderTreeParams,
+  EditorNavTreeParams,
   PageNavTreeParams
 }
