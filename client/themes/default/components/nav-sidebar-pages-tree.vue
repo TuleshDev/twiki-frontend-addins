6979160d9acd3890a8f81c9aceb2e2bfe70168b4
tulesh <tulesh.hub@gmail.com>
2024-08-09 18:55:47 +0500

    Replaced v-list-item elements with v-treeview elements with modified settings in the 'nav-sidebar-pages-tree.vue' component

--- client/themes/default/components/nav-sidebar-pages-tree.vue
+++ client/themes/default/components/nav-sidebar-pages-tree.vue
@@ -1,16 +1,18 @@
 <template lang="pug">
-  div
+  div.nav-sidebar-pages-tree
     //-> Custom Navigation
     template(v-if='currentMode === `custom`')
-      v-list-item(
-        :href='item.t'
-        :target='item.y === `externalblank` ? `_blank` : `_self`'
-        :rel='item.y === `externalblank` ? `noopener` : ``'
-        )
-        v-list-item-avatar(size='24', tile)
-          v-icon(v-if='item.c.match(/fa[a-z] fa-/)', size='19') {{ item.c }}
-          v-icon(v-else) {{ item.c }}
-        v-list-item-title {{ item.l }}
+      //v-list-item(
+      //  v-if='item.k === `link`'
+      //  :href='item.t'
+      //  :target='item.y === `externalblank` ? `_blank` : `_self`'
+      //  :rel='item.y === `externalblank` ? `noopener` : ``'
+      //  style='min-height: 30px;'
+      //  )
+      //  v-list-item-avatar(size='18', tile, :style='`padding-left: 0px; width: auto; margin: 0 5px 0 0;`')
+      //    v-icon(v-if='item.c.match(/fa[a-z] fa-/)', size='19') {{ item.c }}
+      //    v-icon(v-else small) {{ item.c }}
+      //  v-list-item-title {{ item.l }}
       v-treeview(
         v-if='item.k === `link`'
         v-model='tree'
@@ -19,6 +21,7 @@
         item-key='name'
         activatable
         open-on-click
+        style='min-height: 30px;'
         )
         template(v-slot:prepend='{ item, open }')
           v-list-item(
@@ -26,17 +29,18 @@
             :href='item.t'
             :target='item.y === `externalblank` ? `_blank` : `_self`'
             :rel='item.y === `externalblank` ? `noopener` : ``'
+            style='min-height: 30px;'
             )
-            v-list-item-avatar(size='24', tile)
+            v-list-item-avatar(size='18', tile, :style='`padding-left: 0px; width: auto; margin: 0 5px 0 0;`')
               v-icon(v-if='item.c.match(/fa[a-z] fa-/)', size='19') {{ item.c }}
-              v-icon(v-else) {{ item.c }}
+              v-icon(v-else small) {{ item.c }}
             v-list-item-title {{ item.l }}
     //-> Browse
     template(v-if='currentMode === `browse`')
-      v-list-item(:href='`/` + item.locale + `/` + item.path', :key='`childpage-` + item.id', :input-value='path === item.path')
-        v-list-item-avatar(size='24')
-          v-icon mdi-text-box
-        v-list-item-title {{ item.title }}
+      //v-list-item(:href='`/` + item.locale + `/` + item.path', :key='`childpage-` + item.id', :input-value='path === item.path', style='min-height: 30px;')
+      //  v-list-item-avatar(size='18', :style='`padding-left: 0px; width: auto; margin: 0 5px 0 0;`')
+      //    v-icon(small) mdi-text-box
+      //  v-list-item-title {{ item.title }}
       v-treeview(
         v-model='tree'
         :items='items2'
@@ -44,11 +48,12 @@
         item-key='name'
         activatable
         open-on-click
+        style='min-height: 30px;'
         )
         template(v-slot:prepend='{ item, open }')
-          v-list-item(:href='`/` + item.locale + `/` + item.path', :key='`childpage-` + item.id', :input-value='path === item.path')
-            v-list-item-avatar(size='24')
-              v-icon mdi-text-box
+          v-list-item(:href='`/` + item.locale + `/` + item.path', :key='`childpage-` + item.id', :input-value='path === item.path', style='min-height: 30px;')
+            v-list-item-avatar(size='18', :style='`padding-left: 0px; width: auto; margin: 0 5px 0 0;`')
+              v-icon(small) mdi-text-box
             v-list-item-title {{ item.title }}
 </template>
 
@@ -131,9 +136,23 @@ export default {
 
 <style lang="scss">
 
-.v-treeview-node__content {
-  display: block;
-  color: #fff;
+.nav-sidebar-pages-tree {
+  .v-treeview-node__root {
+    min-height: 37px;
+  }
+  .v-treeview-node__content {
+    display: block;
+    color: #fff;
+  }
+  .v-treeview-node__level, .v-treeview-node__toggle {
+    width: 18px;
+  }
+  .v-treeview-node__root > .v-icon.v-icon {
+    font-size: 18px;
+  }
+  .v-treeview-node__root .v-list-item {
+    padding: 0 5px;
+  }
 }
 
 </style>
